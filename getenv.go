98a5a45a1162264d6786e986423de137d37bab75
Deniss Marinuks <dmarinuks@gmail.com>
2020-08-04 18:32:06 +0200

    added returns insted of pointer, added slice loaders

--- getenv.go
+++ getenv.go
@@ -1,6 +1,7 @@
 package getenv
 
 import (
+	"errors"
 	"log"
 	"os"
 	"strconv"
@@ -76,7 +77,7 @@ func logMsg(fatal bool, args ...interface{}) {
 }
 
 // Int loads env key and returns it as an int value
-func Int(key string, variable *int, fatal bool) {
+func Int(key string, fatal bool) (int, error) {
 	logMsg(false, "GetEnv Key:", key, "=", os.Getenv(key))
 	if val := os.Getenv(key); len(val) > 0 {
 		i, err := strconv.Atoi(val)
@@ -84,35 +85,103 @@ func Int(key string, variable *int, fatal bool) {
 			if fatal {
 				logMsg(fatal, "Could not load ENV", key)
 			}
+			return -1, err
 		}
-		*variable = i
-	} else {
-		if fatal {
-			logMsg(fatal, "Could not load ENV", key)
-		}
+		return i, nil
 	}
+	if fatal {
+		logMsg(fatal, "Could not load ENV", key)
+	}
+	return -1, errors.New("environment variable not found")
 }
 
 // Bool loads env key and returns it as a bool value
-func Bool(key string, variable *bool, fatal bool) {
+func Bool(key string, fatal bool) (bool, error) {
 	logMsg(false, "GetEnv Key:", key, "=", os.Getenv(key))
-	if val := os.Getenv(key); len(val) > 0 {
-		*variable = strings.ToLower(val) != "false"
-	} else {
-		if fatal {
-			logMsg(fatal, "Could not load ENV", key)
+	if val := strings.ToLower(os.Getenv(key)); len(val) > 0 {
+		if val != "false" && val != "true" {
+			return false, errors.New("not bool value")
 		}
+		return strings.ToLower(val) == "true", nil
 	}
+	if fatal {
+		logMsg(fatal, "Could not load ENV", key)
+	}
+	return false, errors.New("environment variable not found")
 }
 
 // Str loads env key and returns it as a string value
-func Str(key string, variable *string, fatal bool) {
+func Str(key string, fatal bool) string  {
 	logMsg(false, "GetEnv Key:", key, "=", os.Getenv(key))
 	if val := os.Getenv(key); len(val) > 0 {
-		*variable = val
-	} else {
-		if fatal {
-			logMsg(fatal, "Could not load ENV", key)
+		return val
+	}
+	if fatal {
+		logMsg(fatal, "Could not load ENV", key)
+	}
+	return ""
+}
+
+// StrSlice takes ENV_KEY string, separator string, variable []string, fatal bool
+// It return slice of strings
+func StrSlice(key string, separator string, fatal bool) []string {
+	logMsg(false, "GetEnv Key:", key, "=", os.Getenv(key))
+	var results []string
+	if val := os.Getenv(key); len(val) > 0 {
+		arr := strings.Split(val, separator)
+		for _, v := range arr {
+			results = append(results, v)
+		}
+		return results
+	}
+	if fatal {
+		logMsg(fatal, "Could not load ENV", key)
+	}
+	return results
+}
+
+// IntSlice takes ENV_KEY string, separator string, variable []int, fatal bool
+// It return slice of strings
+func IntSlice(key string, separator string, fatal bool) ([]int, error){
+	logMsg(false, "GetEnv Key:", key, "=", os.Getenv(key))
+	var results []int
+	if val := os.Getenv(key); len(val) > 0 {
+		arr := strings.Split(val, separator)
+		for _, v := range arr {
+			i, err := strconv.Atoi(v)
+			if err != nil {
+				if fatal {
+					logMsg(fatal, "Could not load ENV", key)
+				}
+				return results, err
+			}
+			results = append(results, i)
 		}
 	}
+	if fatal {
+		logMsg(fatal, "Could not load ENV", key)
+	}
+	return results, errors.New("environment variable not found")
+}
+
+// BoolSlice takes ENV_KEY string, separator string, variable []bool, fatal bool
+// It return slice of strings
+func BoolSlice(key string, separator string, fatal bool) ([]bool, error){
+	logMsg(false, "GetEnv Key:", key, "=", os.Getenv(key))
+	var results []bool
+	if val := os.Getenv(key); len(val) > 0 {
+		arr := strings.Split(val, separator)
+		for _, v := range arr {
+			v = strings.ToLower(v)
+			if v != "false" && v != "true" {
+				return results, errors.New("not bool value")
+			}
+			results = append(results, strings.ToLower(v) == "true")
+		}
+		return results, nil
+	}
+	if fatal {
+		logMsg(fatal, "Could not load ENV", key)
+	}
+	return results, errors.New("environment variable not found")
 }
--- getenv_test.go
+++ getenv_test.go
@@ -8,8 +8,7 @@ import (
 func TestInt(t *testing.T) {
 	os.Setenv("FOO", "123")
 	want := 123
-	got := 0
-	Int("FOO", &got, false)
+	got, _ := Int("FOO",false)
 	if got != want {
 		t.Errorf("GetEnvInt() = %v, want %v", got, want)
 	}
@@ -18,8 +17,7 @@ func TestInt(t *testing.T) {
 func TestBool(t *testing.T) {
 	os.Setenv("FOO", "true")
 	want := true
-	got := false
-	Bool("FOO", &got, false)
+	got, _ := Bool("FOO", false)
 	if got != want {
 		t.Errorf("GetEnvBool() = %v, want %v", got, want)
 	}
@@ -28,9 +26,35 @@ func TestBool(t *testing.T) {
 func TestStr(t *testing.T) {
 	os.Setenv("FOO", "BAR")
 	want := "BAR"
-	got := ""
-	Str("FOO", &got, false)
+	got := Str("FOO", false)
 	if got != want {
+		t.Errorf("GetEnvInt() = %v, want %v", got, want)
+	}
+}
+
+func TestStrSlice(t *testing.T) {
+	os.Setenv("FOO", "BAR, CAT")
+	want := []string{"BAR", "CAT"}
+	got := StrSlice("FOO",",", false)
+	if len(got) == 0 || (got[0] != want[0] && got[1] != want[1]) {
+		t.Errorf("GetEnvInt() = %v, want %v", got, want)
+	}
+}
+
+func TestIntSlice(t *testing.T) {
+	os.Setenv("FOO", "1, 2")
+	want := []int{1, 2}
+	got, _ := IntSlice("FOO",",", false)
+	if len(got) == 0 || (got[0] != want[0] && got[1] != want[1]) {
+		t.Errorf("GetEnvInt() = %v, want %v", got, want)
+	}
+}
+
+func TestBoolSlice(t *testing.T) {
+	os.Setenv("FOO", "True, false")
+	want := []bool{true, false}
+	got, _ := BoolSlice("FOO",",", false)
+	if len(got) == 0 || (got[0] != want[0] && got[1] != want[1]) {
 		t.Errorf("GetEnvInt() = %v, want %v", got, want)
 	}
 }
